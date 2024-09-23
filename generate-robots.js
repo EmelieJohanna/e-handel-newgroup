9ab918eb5e658f3a9c6386b998075b7b81e35456
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-09-23 17:49:24 +0200

    Fixed issue with incorrect path by using __dirname workaround for ES modules + SourcePath is now being passed to fs.copyFile

--- generate-robots.js
+++ generate-robots.js
@@ -1,5 +1,10 @@
+import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
+
+// Since __dirname not available in ES modules => use import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const env = process.env.NODE_ENV || "production";
 
@@ -7,7 +12,7 @@ const sourceFile = env === "development" ? "robots.dev.txt" : "robots.prod.txt";
 const sourcePath = path.join(__dirname, "public", "robots", sourceFile);
 const destinationPath = path.join(__dirname, "public", "robots.txt");
 
-fs.copyFile(sourceFile, destinationPath, (err) => {
+fs.copyFile(sourcePath, destinationPath, (err) => {
   if (err) {
     console.error("Error copying robots.txt", err);
   } else {
